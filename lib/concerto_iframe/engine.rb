765c504b42329ce2ded713c0d520fd5210702d50
gabe283 <perez283@gmail.com>
2015-05-05 21:38:33 -0400

    Update plugin to support new frontend

--- lib/concerto_iframe/engine.rb
+++ lib/concerto_iframe/engine.rb
@@ -5,5 +5,12 @@ module ConcertoIframe
     initializer "register content type" do |app|
       app.config.content_types << Iframe
     end
+
+    # Define plugin information for the Concerto application to read
+    def plugin_info(plugin_info_class)
+      @plugin_info ||= plugin_info_class.new do
+        add_view_hook "frontend/ScreensController", :concerto_frontend_plugins, partial: "frontend/concerto_iframe.html"
+      end
+    end
   end
 end
